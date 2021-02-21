50d0e47c28e44916e7ed430d62ab21bb56b7453a
Enes Güngör <70583416+CoderEnesG@users.noreply.github.com>
2021-02-21 19:28:08 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,40 @@
-#Explain your work
+#2. ÖDEV
+# Enes GÜNGÖR
+# enesgungor.gs@gmail.com
 
-#Question 1
-for x in range(a):
-	print(a)
+
+
+students = []
+pointItems = ['homework', 'midterm', 'final']
+# Burada not verilecek kategorileri seçiyoruz
+points = []
+
+needStudentNum = 5
+# Kaç öğrenci arasında sıralama yapacağımız
+
+whileOperator = 0
+
+while(whileOperator < needStudentNum):
+    studentName = str(input(f"{whileOperator+1}. Write Student Name: "))
+    students.append(studentName)
+
+    point = []
+    for i in pointItems:
+        point.append(int(input(f"Write {i}: ")))
+
+    totalPoint = 0
+    # Toplam puanı alır (Ortalama işleminde kullanmak için)
+    for j in point:
+        totalPoint += j
+    points.append(totalPoint/len(point))
+    # Bütün ortalamaları listeye ekler
+    whileOperator += 1
+
+info = dict(sorted(zip(points, students)))
+info = dict((value, key) for (key, value) in info.items())
+
+
+for k, v in info.items():
+    print(f"{k} :  {v}")
+
+print(f"**** Congratulations !!! : {students[points.index(max(points))]} ")
