d96428966e65a7ce59692294b624974482bb6a1a
Enes Güngör <70583416+CoderEnesG@users.noreply.github.com>
2021-02-21 19:30:51 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -2,27 +2,27 @@
 # Enes GÜNGÖR
 # enesgungor.gs@gmail.com
 
-def prime_first(number):
+def asal_first(number):
   for j in range(2,number):
     if number % j == 0:
       return None 
   else:
-    return number, "prime_first"
+    return number, "asal_first"
 
 
-def prime_second(number):
+def asal_second(number):
   for j in range(2,number):
     if number % j == 0:
       return None
   else:
-    return number, "prime_second"
+    return number, "asal_second"
 
 for i in range(0,1000):
   if i == 0 or i == 1:
     continue 
   elif i < 500:
-    if prime_first(i) != None:
-      print(prime_first(i))
+    if asal_first(i) != None:
+      print(asal_first(i))
   else:
-    if prime_second(i) != None:
-      print(prime_second(i))
+    if asal_second(i) != None:
+      print(asal_second(i))
