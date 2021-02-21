e83a1e7aa3e32ea73d1674588410efebd44edfb1
Enes Güngör <70583416+CoderEnesG@users.noreply.github.com>
2021-02-21 19:28:35 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,28 @@
-#Explain your work
+#3. ödev
+# Enes GÜNGÖR
+# enesgungor.gs@gmail.com
 
-#Question 1
-for x in range(a):
-	print(a)
+def prime_first(number):
+  for j in range(2,number):
+    if number % j == 0:
+      return None 
+  else:
+    return number, "prime_first"
+
+
+def prime_second(number):
+  for j in range(2,number):
+    if number % j == 0:
+      return None
+  else:
+    return number, "prime_second"
+
+for i in range(0,1000):
+  if i == 0 or i == 1:
+    continue 
+  elif i < 500:
+    if prime_first(i) != None:
+      print(prime_first(i))
+  else:
+    if prime_second(i) != None:
+      print(prime_second(i))
